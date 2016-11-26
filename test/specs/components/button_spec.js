e21dec0e06b8d1288729fd0f7b6af16e5f18c7f5
David Singleton <david@dsingleton.co.uk>
2016-11-26 10:37:20 +0000

    Improve component test spec DSL
    automatically dedent the template string whitespace, allowing the spec to
    use normal indentation

--- test/specs/components/button_spec.js
+++ test/specs/components/button_spec.js
@@ -1,31 +1,43 @@
 const expect = require('chai').expect
 const nunjucks = require('nunjucks')
+const dedent = require('dedent-js')
 
 const componentPath = 'app/components/button.html.nunjucks'
-const expectRender = (input, expected) => expect(nunjucks.render(componentPath, input)).to.equal(expected)
+const expectComponent = (input, expected) => {
+  expect(
+    nunjucks.render(componentPath, input)
+  ).to.equal(
+    // Remove the additional template string indentation
+    // Extra new line to match rendered ouput
+    dedent(expected) + '\n'
+  )
+}
 
 describe('Button component', function () {
-  it('should render', function (done) {
-    expectRender({text: 'hello!'}, `<button class="gv-c-button">
-  hello!
-</button>
-`)
-    done()
+  it('should render', () => {
+    expectComponent(
+      { text: 'hello!' },
+      `<button class="gv-c-button">
+        hello!
+      </button>`
+    )
   })
 
-  it('should render primary', function (done) {
-    expectRender({text: 'hello!', isPrimary: true}, `<button class="gv-c-button gv-c-button--primary">
-  hello!
-</button>
-`)
-    done()
+  it('should render primary', function () {
+    expectComponent(
+      { text: 'hello!', isPrimary: true },
+      `<button class="gv-c-button gv-c-button--primary">
+        hello!
+      </button>`
+    )
   })
 
-  it('should render type', function (done) {
-    expectRender({text: 'hello!', type: 'submit'}, `<button class="gv-c-button" type="submit">
-  hello!
-</button>
-`)
-    done()
+  it('should render type', function () {
+    expectComponent(
+      {text: 'hello!', type: 'submit'},
+      `<button class="gv-c-button" type="submit">
+        hello!
+      </button>`
+    )
   })
 })
