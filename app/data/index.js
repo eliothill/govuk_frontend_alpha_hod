9c89df2b0f4adb6bf0623c25fb75cb0858d6bc6d
David Singleton <david@dsingleton.co.uk>
2016-11-27 11:45:19 +0000

    Make component data available from module
    Avoids dynamic requires, allows access to all component data

---
+++ app/data/index.js
@@ -0,0 +1,7 @@
+const components = {
+  'button': require('./button'),
+  'form-group': require('./form-group'),
+  'form-radio-group': require('./form-radio-group')
+}
+
+module.exports = components
